dfb03cad43c1563d4639416c80b1c1f7734d1ad7
the-viking <erb78@cornell.edu>
2013-07-31 15:53:45 -0600

    created\ new app for sticky note functionality

---
+++ stickies/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from stickies.models import Sticky
+
+
+admin.site.register(Sticky)
---
+++ stickies/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Sticky(models.Model):
+    
+    writer = models.ForeignKey(User)
+    message = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True, blank=True)
